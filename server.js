b60b69201eafc028759ab6f1a2b236715e18a9fd
ANKITA-RATHORE38 <an96173420@gmail.com>
2026-09-01 17:54:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+const bodyparser=require('body-parser');
+app.use(bodyparser.json());
+const PORT= process.env.PORT||3000;
+
+
+// Import the router files
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
+// Use the routers
+app.use('/user', userRoutes);
+app.use('/candidate',candidateRoutes);
+app.listen(PORT,()=>{
+    console.log("listening on port 3000");
+})
